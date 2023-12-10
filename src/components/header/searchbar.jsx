73532c56113cf44a81c663ecdabd6f782d1a9d5a
irmbyznr <irmbyznr@gmail.com>
2023-12-10 15:53:22 +0300

    lastversion

--- src/components/header/searchbar.jsx
+++ src/components/header/searchbar.jsx
@@ -130,11 +130,11 @@ export const SearchBar = () => {
             ))}
           </div>
           <div className="lg:flex items-center gap-4 hidden pr-20 pt-2">
-            <Badge badgeContent={1} className="text-xl" color="secondary">
+            <Badge badgeContent={1} className="text-xl" style={{ backgroundColor: 'yellow' }}>
               <BsHeart />
             </Badge>
             <span className="text-border ">|</span>
-            <Badge badgeContent={1} className="text-2xl" color="secondary">
+            <Badge badgeContent={1} className="text-2xl" style={{ backgroundColor: 'yellow' }}>
               <LiaShoppingBagSolid />
             </Badge>
           </div>
